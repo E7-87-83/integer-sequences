52e74ed346ff479bf34273d67aaa8bba773daaed
Fung Cheok Yin <61836418+E7-87-83@users.noreply.github.com>
2021-07-16 16:12:00 +0800

    Update and rename bases.pl to 31bases.pl

--- 31bases.pl
+++ 31bases.pl
@@ -8,6 +8,11 @@ use v5.10;
 
 # 3 <= a(n) <= n-2 for n > 4
 
+# Question:
+# I. For a positive integer s > 2, does it exists 
+# positive integer m such that a(n) > s for all n > m?
+#II. For any positive integer s > 2, does it always exists
+# a positive integer m such that a(m) = s ?
 
 my $UPPER = $ARGV[0] || 20;
 
