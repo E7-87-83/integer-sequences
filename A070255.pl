d84e49f13d3fcba83420f274c3a077888a1c264d
Fung Cheok Yin <61836418+E7-87-83@users.noreply.github.com>
2021-08-11 23:48:31 +0800

    Create A070255.pl

--- A070255.pl
+++ A070255.pl
@@ -23,8 +23,40 @@ sub sooo {
 
 }
 
-# A New Conjecture: If no zeros is allowed, the sequence is finite.
-# Another conjecture: are all very large terms one of the form 
-# d*10^n + d , where d goes from 1 to 9? i.e.
-# 10...01, 20...02, 30...03, 40...04, 
-# 50...05, 60...06, 70...07, 80..08, 90..09 ?
+# Some forms:
+# 10...01,
+# 10...02
+# 10...05
+# 12...01
+# 12...02
+# 12...03
+# 12...06
+# 13...01
+# 14...01
+# 14...02
+# 14...07
+# 15...01
+# 15...03
+# 15...05
+# 16...01
+# 16...02
+# 16...08
+# 17...01
+# 17...51
+# 18...01
+# 18...02
+# 18...03
+# 18...06
+# 18...09
+# 19...01
+# 20...01
+# 20...02
+# 20...01
+# 20...02
+# 20...04
+# 30...03, 40...04, 
+# 50...05, 60...06, 70...07, 80..08, 90..09 ...
+
+
+# A New Conjecture: If no zeros are allowed, the sequence is finite.
+# This Look like easy to prove.
