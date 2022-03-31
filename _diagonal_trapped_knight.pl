d8b657a2ccd4a404448de0da63b16c9c8a843b33
Fung Cheok Yin <61836418+E7-87-83@users.noreply.github.com>
2022-03-31 08:36:22 +0800

    Update _diagonal_trapped_knight.pl

--- _diagonal_trapped_knight.pl
+++ _diagonal_trapped_knight.pl
@@ -16,7 +16,28 @@
 
    . 
 
+or
+    5  6
+12  1  2  7
+11  4  3  8
+   10  9
+
+(Aztec diamond I)
+
+
+    7  8
+ 6  1  2  9
+ 5  4  3 10
+   12 11
+
+
+(spiral Aztec diamond II)
+
+
+
 similar to A316588
 
+https://oeis.org/wiki/Knight_tours
 https://scipython.com/blog/the-trapped-knight
+
 =cut
