1e52f31c52184af5e61bba6b10f3b749d7d59ed0
Fung Cheok Yin <61836418+E7-87-83@users.noreply.github.com>
2022-03-31 08:28:29 +0800

    Add files via upload

---
+++ diagonal.pl
@@ -0,0 +1,22 @@
+
+
+=pod
+
+   1  2  6  7 15 16  .
+
+   3  5  8 14 17  .
+
+   4  9 13 18  .
+
+  10 12 19  .
+
+  11 20  .
+
+  21  .
+
+   . 
+
+similar to A316588
+
+https://scipython.com/blog/the-trapped-knight
+=cut
